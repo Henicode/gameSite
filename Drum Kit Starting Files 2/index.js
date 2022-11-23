020ac930ce6438c3954b8bda20afa104c290ed1b
Henok Ayalew <82248556+Henicode@users.noreply.github.com>
2022-11-23 17:42:22 -0500

    animation

--- Drum Kit Starting Files 2/index.js
+++ Drum Kit Starting Files 2/index.js
@@ -13,13 +13,17 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
         var buttonInnerHTML = this.innerHTML;
+
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 // detecting key pressed 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 
@@ -67,7 +71,16 @@ function makeSound(key) {
     }
 }
 
+function buttonAnimation(currentKey) {
 
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+    
+}
 
 
     
